b90b4dc221fa7c7d5c7c9112a8d6bc5775df9e96
natis25 <natalia.urrutia1325@gmail.com>
2026-03-26 18:58:22 -0400

    Pagina de ofertas laborales (modo catalogo)

--- nexa_careers/src/frontend/router/index.js
+++ nexa_careers/src/frontend/router/index.js
@@ -8,6 +8,7 @@ import RegistroEmpleador from '@/views/RegistroEmpleador.vue'
 import RegistroSupervisor from '@/views/RegistroSupervisor.vue'
 
 //Ofertas y postulantes
+import CatalogoOfertas from '@/views/CatalogoOfertas.vue'
 import MisOfertas from '@/views/MisOfertas.vue'
 import NuevaOferta from '@/views/NuevaOferta.vue'
 import ListaPostulantes from '@/views/ListaPostulantes.vue'
@@ -56,6 +57,7 @@ const routes = [
   
   { path: '/registro-supervisor', name: 'RegistroSupervisor', component: RegistroSupervisor, meta: { soloPublico: true } },
 
+  { path: '/ofertas', name: 'CatalogoOfertas', component: CatalogoOfertas, meta: { soloPublico: true } },
   { path: '/mis-ofertas', name: 'MisOfertas', component: MisOfertas, meta: { requiereRol: ['empleador'] } },
   { path: '/mis-ofertas/nueva', name: 'NuevaOferta', component: NuevaOferta, meta: { requiereRol: ['empleador'] } },
   { path: '/mis-ofertas/:ofertaId/postulantes', name: 'ListaPostulantes', component: ListaPostulantes, meta: { requiereRol: ['empleador'] } },
@@ -66,13 +68,11 @@ const routes = [
 
   //Perfiles
   { path: '/perfil-estudiante', name: 'PerfilEstudiante', component: PerfilEstudiante },
-  { path: '/perfil-empleador', name: 'PerfilEmpleador', component: PerfilEmpleador,
-    // meta: { requiereRol: ['empleador'] }
+  { path: '/perfil-empleador', name: 'PerfilEmpleador', component: PerfilEmpleador, meta: { requiereRol: ['empleador'] }
   },
 
   //Dashboard Supervisor
-  { path: '/dashboard-supervisor', name: 'DashboardSupervisor', component: DashboardSupervisor, 
-    //meta: { requiereRol: ['supervisor'] } 
+  { path: '/dashboard-supervisor', name: 'DashboardSupervisor', component: DashboardSupervisor, meta: { requiereRol: ['supervisor'] } 
   }
 ]
 
