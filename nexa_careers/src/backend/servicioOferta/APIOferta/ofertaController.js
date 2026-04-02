64966db007cb3f4167b4b8f8fadbd34755cea72b
Santiago Torrez <santiago.torez@ucb.edu.bo>
2026-04-02 19:23:30 -0400

    Cambio temporal a controller de oferta

--- nexa_careers/src/backend/servicioOferta/APIOferta/ofertaController.js
+++ nexa_careers/src/backend/servicioOferta/APIOferta/ofertaController.js
@@ -60,7 +60,7 @@ export const crearOferta = async (req, res) => {
   }
 
   try {
-    const estado = 0; // 0 por defecto al crearse
+    const estado = 1; // 0 por defecto al crearse, pero por ahora es 1 
     const rechazo = ''; // Blanco por defecto
 
     // IMPORTANTE: En el SQL usamos 'id_empleador' para coincidir con tu BD
